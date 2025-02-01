c2fd185b83276edbcaa98d1eef0df731ca402029
David Diniz Dos Santos <ddavid.diniz@gmail.com>
2025-02-01 02:43:54 -0300

    initial commit

---
+++ src/app/api/[...nextauth]/route.ts
@@ -0,0 +1,63 @@
+/** @format */
+
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const handler = NextAuth({
+	providers: [
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				email: {
+					label: 'E-mail',
+					type: 'email',
+					placeholder: 'email@email.com',
+				},
+				password: { label: 'Senha', type: 'password', placeholder: '******' },
+			},
+			async authorize(credentials) {
+				const user = {
+					id: '1',
+					email: 'admin@jdinfo.com.br',
+					password: 'admin@jd',
+				};
+				if (
+					credentials?.email === user.email &&
+					credentials?.password === user.password
+				) {
+					return user; // Retorna o usuário se as credenciais forem válidas
+				}
+
+				return null; // Retorna null se as credenciais forem inválidas
+			},
+		}),
+	],
+	pages: {
+		signIn: '/sign-in',
+		signOut: '/sign-in',
+	},
+	callbacks: {
+		jwt: async ({ token, user, session, account, profile }) => {
+			if (user) {
+				return {
+					...token,
+					...user,
+					...session,
+					...account,
+					...profile,
+				};
+			}
+			return token;
+		},
+		session: async ({ session, token, newSession, user }) => {
+			return {
+				...token,
+				...user,
+				...session,
+				...newSession,
+			};
+		},
+	},
+});
+
+export { handler as GET, handler as POST };
---
+++ src/providers/session-provider.tsx
@@ -0,0 +1,14 @@
+/** @format */
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
+interface AuthSessionProviderProps {
+	children: React.ReactNode;
+}
+
+export default function AuthSessionProvider({
+	children,
+}: AuthSessionProviderProps) {
+	return <SessionProvider>{children}</SessionProvider>;
+}
