e45a5a236a3ee2517ef3c53ef499d469795f426e
David Diniz Dos Santos <ddavid.diniz@gmail.com>
2025-02-24 02:03:03 -0300

    feat: refactor date formatting functions to be asynchronous for improved performance

--- src/app/dashboard/marketing/page.tsx
+++ src/app/dashboard/marketing/page.tsx
@@ -31,13 +31,13 @@ type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 export default async function MarketingPage(props: {
 	searchParams: SearchParams;
 }) {
-	function formattedEndDate() {
+	async function formattedEndDate() {
 		const date = new Date();
 		const endDate = date.toISOString().split('T')[0];
 		return endDate;
 	}
 
-	function formattedStartDate() {
+	async function formattedStartDate() {
 		const date = new Date();
 		date.setDate(date.getDate() - 7);
 		const startDate = date.toISOString().split('T')[0];
@@ -45,8 +45,8 @@ export default async function MarketingPage(props: {
 	}
 
 	const searchParams = await props.searchParams;
-	const startDate = searchParams.startDate || formattedStartDate();
-	const endDate = searchParams.endDate || formattedEndDate();
+	const startDate = searchParams.startDate || (await formattedStartDate());
+	const endDate = searchParams.endDate || (await formattedEndDate());
 	const channelFilter = searchParams.channel || 'all';
 
 	const [responseADS, responseAnalytics] = await Promise.all([
