5452987a02cb3a889d74ef060b23749e63504d19
sirdav1d <davidsdiniz@seventechnologies.cloud>
2025-12-14 19:06:03 -0300

    fix: viewTransition import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from '@/providers/theme-provider';
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
-import { unstable_ViewTransition as ViewTransition } from 'react';
+import { ViewTransition } from 'react';
 
 const montserrat = Montserrat({
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -32,7 +32,9 @@ export default function RootLayout({
 				lang='pt-BR'
 				suppressHydrationWarning
 				suppressContentEditableWarning>
-				<body suppressHydrationWarning className={`${montserrat.className} antialiased`}>
+				<body
+					suppressHydrationWarning
+					className={`${montserrat.className} antialiased`}>
 					<ReactQueryProvider>
 						<ThemeProvider
 							attribute={'class'}
