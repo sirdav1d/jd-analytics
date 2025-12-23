47840a6a4ecbfbf6e0bfe592819c211cf1f05ff0
sirdav1d <davidsdiniz@seventechnologies.cloud>
2025-12-23 15:23:22 -0300

    refactor:remove page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,7 @@
 /** @format */
 
-import { ChevronLeft } from 'lucide-react';
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function NotFound() {
-	return (
-		<div className='min-h-screen w-full flex items-center justify-center text-center p-8'>
-			<div className='space-y-2'>
-				<p className='text-3xl font-bold'>Página não encontrada</p>
-				<p className='text-muted-foreground'>
-					A página que você procura não existe ou foi movida.
-				</p>
-				<Link href={'/'}>
-					Voltar
-					<ChevronLeft />
-				</Link>
-			</div>
-		</div>
-	);
+	redirect('/');
 }
